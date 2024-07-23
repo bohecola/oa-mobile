0a239fba5d133f08e8950e08c7ed4703bdc5ed58
li.kong <bohecola@outlook.com>
2024-07-23 19:47:02 +0800

    :bug: fix: typo

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -10,7 +10,7 @@ const { appTitle } = useGlobSetting()
   <div class="h-screen flex justify-center p-8">
     <div class="w-full flex flex-col">
       <div class="flex flex-col items-center justify-center">
-        <Logo class="mb-8 mt08" />
+        <Logo class="mb-8 mt-8" />
         <div class="text-2xl mb-12 font-black">
           {{ appTitle }}
         </div>
