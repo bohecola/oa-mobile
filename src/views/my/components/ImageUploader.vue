3bad60c3d54e10199ff5e0b25de0c65ff8f8489b
bohecola <bohecola@outlook.com>
2024-07-24 00:51:21 +0800

    ✨ feat: avatar upload

--- src/views/my/components/ImageUploader.vue
+++ src/views/my/components/ImageUploader.vue
@@ -1,9 +1,10 @@
 <script setup lang='ts'>
 import type { UploaderFileListItem } from 'vant'
-import { showFailToast, showLoadingToast, showSuccessToast, showToast } from 'vant'
+import { showFailToast, showLoadingToast } from 'vant'
 import { uploadAvatar } from '@/api/system/user'
 import { useStore } from '@/store'
 
+// 用户状态
 const { user } = useStore()
 
 // 文件读取前
@@ -11,37 +12,43 @@ function beferReadHandler(file: File | File[]) {
   if (['image/jpeg', 'image/png', 'image/jpg'].includes((file as File).type)) {
     return true
   }
-
+  // 提示错误
   showFailToast('请上传正确格式的图片')
   return false
 }
 
 // 文件读取完成
-function afterReadHandler(file: UploaderFileListItem | UploaderFileListItem[]) {
+async function afterReadHandler(file: UploaderFileListItem | UploaderFileListItem[]) {
   file = file as UploaderFileListItem
 
   // 加载中
   const loadingToast = showLoadingToast({
     duration: 0,
     message: '图片上传中...',
-    overlay: true,
-    forbidClick: true,
+    loadingType: 'spinner',
+    wordBreak: 'break-word',
   })
 
-  // 数据
+  // 构建 FormData
   const formData = new FormData()
+  // 文件对象
   formData.append('avatarfile', file.file!, file.file!.name)
 
-  // 上传头像
-  uploadAvatar(formData).then((_) => {
-    file.status = 'done'
-    loadingToast.close()
-    showSuccessToast('上传成功')
+  try {
+    // 上传头像
+    await uploadAvatar(formData)
+    // 更新用户信息
     user.get()
-  }).catch((error) => {
+    // 关闭加载中
     loadingToast.close()
-    showToast((error as Error).message)
-  })
+  }
+  catch (error) {
+    // Toast 默认是单例模式，需先关闭加载中的 Toast 提示才可展示后续的 Toast 提示
+    loadingToast.close()
+    // Toast 默认是单例模式，在接口响应的 Code 为 500 错误时，响应拦截器中已有的 Toast 不会再进行重复提示、而是会结束然后直接执行下面的提示
+    // 在 Code 不为 500 时这里的错误提示就是有意义的，例如 401
+    showFailToast((error as Error).message)
+  }
 }
 </script>
 
