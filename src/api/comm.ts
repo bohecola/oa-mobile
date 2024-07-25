e69cb58d28f458d4a47884fe84c527985573423f
li.kong <bohecola@outlook.com>
2024-07-25 19:15:41 +0800

    :sparkles: feat: user logout

--- src/api/comm.ts
+++ src/api/comm.ts
@@ -1,9 +1,18 @@
 import type { UserInfoVO } from './system/user'
 import request from '@/service/request'
 
+// 获取用户信息
 export function person() {
   return request<UserInfoVO>({
     url: '/system/user/getInfo',
     method: 'get',
+  })
+}
+
+// 注销
+export function userLogout() {
+  return request({
+    url: '/auth/logout',
+    method: 'post',
   })
 }
