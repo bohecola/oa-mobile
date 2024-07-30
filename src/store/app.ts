84a00545f884e4ea48c936ce2e99d22d46107101
li.kong <bohecola@outlook.com>
2024-07-30 17:01:02 +0800

    :recycle: refactor: add designSetting file

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,64 +1,6 @@
+import type { Theme, ThemeMode } from '@/settings/designSetting'
+import { themes } from '@/settings/designSetting'
 import { storage } from '@/utils'
-
-interface Theme {
-  name: string
-  light: string
-  dark: string
-  zh?: string
-}
-
-const themes: Theme[] = [
-  {
-    name: 'green',
-    light: '#16a34a',
-    dark: '#22c55e',
-    zh: '奶绿',
-  },
-  {
-    name: 'violet',
-    light: '#7c3aed',
-    dark: '#6d28d9',
-    zh: '酱紫',
-  },
-  {
-    name: 'rose',
-    light: '#e11d48',
-    dark: '#e11d48',
-    zh: '玫红',
-  },
-  {
-    name: 'yellow',
-    light: '#facc15',
-    dark: '#facc15',
-    zh: '柠黄',
-  },
-  {
-    name: 'blue',
-    light: '#2563eb',
-    dark: '#3b82f6',
-    zh: '海蓝',
-  },
-  {
-    name: 'orange',
-    light: '#f97316',
-    dark: '#ea580c',
-    zh: '橙黄',
-  },
-  {
-    name: 'emerald',
-    light: '#78dec7',
-    dark: '#78dec7',
-    zh: '翠绿',
-  },
-  {
-    name: 'teal',
-    light: '#00c1d4',
-    dark: '#00c1d4',
-    zh: '靛青',
-  },
-]
-
-type ThemeMode = 'light' | 'dark'
 
 // 数据
 const data = storage.info()
