0d8a461ee999edba1da753fd3852c54ad9d69d64
li.kong <bohecola@outlook.com>
2024-09-20 21:14:45 +0800

    :sparkles: feat: UserSelect Search

--- src/components/UserSelect/index.vue
+++ src/components/UserSelect/index.vue
@@ -1,52 +1,85 @@
 <template>
-  <van-popup
-    v-model:show="visible"
-    position="bottom"
-    class="h-full"
-    closeable
-    teleport="body"
-  >
-    <div v-loading="loading">
-      <van-index-bar
-        class="index-bar"
-        :sticky="true"
-        :index-list="indexList"
+  <div>
+    <!-- TODO 未打开 Popup 时，数据回显、挂载时请求数据、注意重复请求 -->
+    <div class="flex flex-wrap justify-end gap-2">
+      <van-tag
+        v-for="selected in selectedList"
+        :key="selected.userId"
+        type="primary"
+        size="large"
       >
-        <template v-for="(list, key, index) in userListMap" :key="index">
-          <van-index-anchor :index="key.toLocaleUpperCase()" />
-          <van-cell
-            v-for="user in list"
-            :key="user.userId"
-            :class="[{ '!bg-[var(--van-primary-color)]': selectedIdList.includes(user.userId) }]"
-            title-class="w-full"
-            @click="handleUserClick(user)"
-          >
-            <template #title>
-              <div :class="`flex items-center ${selectedIdList.includes(user.userId) ? 'text-white' : ''}`">
-                <span class="w-[12ch] truncate">
-                  {{ user.nickName }}
-                </span>
-                <span class="text-xs opacity-60 truncate">{{ user.deptName }}</span>
-              </div>
-            </template>
-          </van-cell>
-        </template>
-      </van-index-bar>
-      <bottom-line v-if="!loading" />
-      <!-- 底部面板高度占位元素 -->
-      <div
-        :style="{
-          height: `${BottomSelectPanelHeight}px`,
-        }"
-      />
+        {{ selected.nickName }}
+      </van-tag>
     </div>
-    <!-- 底部选择面板 -->
-    <div ref="BottomSelectPanel" class="fixed bottom-0 w-full min-h-10 bg-[--bg-secondary-color] z-10">
-      <div class="p-3 flex flex-col gap-2">
+    <van-popup
+      v-model:show="visible"
+      position="bottom"
+      class="h-full"
+      teleport="body"
+      @closed="popupClosed"
+    >
+      <!-- class="h-[calc(100vh-var(--van-nav-bar-height))] overflow-y-auto" -->
+      <!-- <NavBar
+          title="选择联系人"
+          :fixed="true"
+          :is-left-click-back="false"
+          @click-left="close"
+        /> -->
+      <!-- :sticky-offset-top="46" -->
+      <div v-loading="loading" class="">
+        <!-- 搜索 -->
+        <van-search
+          v-model="searchText"
+          placeholder="搜索"
+          :show-action="isSearchFocused"
+          @focus="handleSearchFocus"
+          @cancel="handleSearchCancel"
+        />
+        <!-- 索引栏 -->
+        <div v-show="!isSearchFocused">
+          <van-index-bar
+            class="index-bar"
+            :sticky="true"
+            :index-list="indexList"
+          >
+            <template v-for="(list, key, index) in userListMap" :key="index">
+              <van-index-anchor :index="key.toLocaleUpperCase()" />
+              <UserCell
+                v-for="user in list"
+                :key="user.userId"
+                :is-active="selectedIdList.includes(user.userId)"
+                :user="user"
+                @click="handleUserCellClick(user)"
+              />
+            </template>
+          </van-index-bar>
+          <!-- 底线 -->
+          <bottom-line v-if="!loading" />
+          <!-- 底部面板占位元素 -->
+          <div
+            :style="{
+              height: `${BottomSelectPanelHeight}px`,
+            }"
+          />
+        </div>
+        <!-- 搜索列表 -->
+        <div v-show="isSearchFocused" class="h-[calc(100vh-var(--van-search-input-height)-20px-128px)] pb-10 overflow-y-auto">
+          <UserCell
+            v-for="user in searchList"
+            :key="user.userId"
+            :is-active="selectedIdList.includes(user.userId)"
+            :user="user"
+            @click="handleUserCellClick(user)"
+          />
+        </div>
+      </div>
+
+      <!-- 底部选择面板 -->
+      <div ref="BottomSelectPanel" class="w-full h-32 bg-[--bg-secondary-color] p-3 flex flex-col justify-between gap-2">
         <div v-if="multiple" class="text-sm">
           已选择 {{ selectedNum }} 人
         </div>
-        <div v-if="hasSelected" class="flex gap-2 flex-wrap">
+        <div v-if="hasSelected" class="flex gap-2 flex-wrap overflow-y-auto">
           <van-tag
             v-for="selected in selectedList"
             :key="selected.userId"
@@ -58,13 +91,14 @@
             {{ selected.nickName }}
           </van-tag>
         </div>
+        <!-- 操作按钮 -->
         <div class="flex justify-end gap-2">
           <van-button
             class="w-20"
             type="default"
             size="small"
             round
-            @click="visible = false"
+            @click="close"
           >
             取消
           </van-button>
@@ -79,11 +113,13 @@
           </van-button>
         </div>
       </div>
-    </div>
-  </van-popup>
+    </van-popup>
+  </div>
 </template>
 
 <script setup lang='ts'>
+import { cloneDeep, isArray, isNumber, isObject, isString } from 'lodash-es'
+import UserCell from './cell.vue'
 import type { SysUserMobileVO } from '@/api/system/user/types'
 import { service } from '@/service'
 
@@ -115,6 +151,19 @@ const selectedNum = computed(() => selectedList.value.length)
 const hasSelected = computed(() => selectedList.value.length > 0)
 const userList = computed(() => Object.values(userListMap.value).flat())
 
+// 搜索
+const searchText = ref('')
+const isSearchFocused = ref(false)
+const searchList = computed(() => {
+  const hasSearchText = searchText.value !== ''
+
+  if (hasSearchText) {
+    return userList.value.filter(e => e.nickName?.includes(searchText.value))
+  }
+
+  return []
+})
+
 // 索引列表
 const indexList = computed(() => Object.keys(userListMap.value).map(e => e.toLocaleUpperCase()))
 
@@ -123,6 +172,16 @@ function open() {
   userListMap.value = {}
   visible.value = true
   getList()
+}
+
+// 关闭
+function close() {
+  visible.value = false
+}
+
+// 弹窗关闭动画结束后
+function popupClosed() {
+  searchText.value = ''
 }
 
 // 请求人员列表
@@ -138,7 +197,7 @@ async function getList() {
 }
 
 // 选择
-function handleUserClick(user: SysUserMobileVO) {
+function handleUserCellClick(user: SysUserMobileVO) {
   const index = selectedIdList.value.findIndex(e => e === user.userId)
   const isSelected = index !== -1
   // 单选
@@ -167,55 +226,57 @@ function handleRemoveUser(user: SysUserMobileVO) {
 // 确定
 function confirm() {
   visible.value = false
-  const payload = getValuesByType()
+  const payload = getValues()
   emit('confirm', payload)
   emit('update:modelValue', payload)
 }
 
-// 获取选中值
-function getValues() {
-  // 单选
-  if (!props.multiple) {
-    return selectedList.value[0]
+// 判断是否为字符串或数字类型数组
+function isStringOrNumberArray(arr: any[]) {
+  return arr.every(e => isString(e) || isNumber(e))
+}
+
+// 转换 modelValue 为 id 数组
+function transformValue(value: typeof props.modelValue) {
+  if (isArray(value)) {
+    if (isStringOrNumberArray(value)) {
+      return value
+    }
+    else {
+      return (value as SysUserMobileVO[]).map(e => e.userId)
+    }
   }
-  // 多选
   else {
-    return selectedList.value
+    if (isObject(value)) {
+      return [value.userId]
+    }
+    else {
+      return [value]
+    }
   }
 }
 
 // 根据类型获取选中值
-function getValuesByType() {
-  const values = getValues()
-  // 返回选中 value | value[]
-  if (props.valueType === 'value') {
-    // 单选
-    if (!props.multiple) {
-      return (values as SysUserMobileVO).userId
-    }
-    // 多选
-    else {
-      return (values as SysUserMobileVO[]).map(e => e.userId)
-    }
+function getValues() {
+  const cloneList = cloneDeep(selectedList.value)
+  // 单选
+  if (!props.multiple) {
+    const [item] = cloneList
+    return props.valueType === 'value' ? item.userId : item
   }
-  // 返回选中 object | object[]
-  return values
+  // 多选
+  else {
+    return props.valueType === 'value' ? cloneList.map(e => e.userId) : cloneList
+  }
 }
 
-// 单选回显数据查询
-function findUser(value: SysUserMobileVO['userId'] | SysUserMobileVO) {
-  if (props.valueType === 'value') {
-    return userList.value.find(e => e.userId === value)
-  }
-  return userList.value.find(e => e.userId === (value as SysUserMobileVO).userId)
+function handleSearchFocus() {
+  isSearchFocused.value = true
 }
 
-// 多选回显数据查询
-function filterUsers(values: SysUserMobileVO['userId'][] | SysUserMobileVO[]) {
-  if (props.valueType === 'value') {
-    return userList.value.filter(e => (values as SysUserMobileVO['userId'][]).includes(e.userId))
-  }
-  return userList.value.filter(e => (values as SysUserMobileVO[]).map(e => e.userId).includes(e.userId))
+function handleSearchCancel() {
+  isSearchFocused.value = false
+  searchText.value = ''
 }
 
 // 回显
@@ -223,15 +284,12 @@ watch(
   [() => props.modelValue, userList],
   ([value]) => {
     console.log(value, 'user-select:watch')
-    // 单选
-    if (!props.multiple) {
-      const user = findUser(value)
-      selectedList.value = user ? [user] : []
-    }
-    // 多选
-    else {
-      selectedList.value = filterUsers(value as SysUserMobileVO['userId'][] | SysUserMobileVO[])
-    }
+
+    // 绑定值转为 id 数组
+    const ids = transformValue(value)
+
+    // 根据 id 数组计算已选择的列表
+    selectedList.value = userList.value.filter(e => ids.includes(e.userId))
 
     // 设置底部选择面板占位元素的高度
     nextTick(() => {
@@ -244,3 +302,10 @@ defineExpose({
   open,
 })
 </script>
+
+<style lang="css">
+.safe {
+  --safe-area-inset-bottom: env(safe-area-inset-bottom);
+  padding-bottom: var(--safe-area-inset-bottom)
+}
+</style>
