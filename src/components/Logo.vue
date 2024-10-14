ad72064259e38ea3321ff1515196575992684ba1
li.kong <bohecola@outlook.com>
2024-10-14 17:01:17 +0800

    :bug: fix: add logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="w-24 h-24 flex items-center justify-center rounded bg-indigo-500 text-white">
-    Logo
+  <div class="w-24 h-24 flex items-center justify-center rounded bg-[var(--bg-color)]">
+    <img :src="logo" class="object-contain" alt="Logo">
   </div>
 </template>
+
+<script setup lang="ts">
+import logo from '@/assets/images/logo.png'
+</script>
