5bbe09c780068a94be44eef1a596366135869ccb
li.kong <bohecola@outlook.com>
2025-01-04 14:33:10 +0800

    :sparkles: feat: add isNegative util func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,6 +11,19 @@ export function filterTruthyKeys<T extends Record<string, any>>(obj: Partial<Rec
   return Object.keys(obj).filter(key => obj[key]) as (keyof T)[]
 }
 
+// 是否为负数
+export function isNegative(value?: number | string): boolean {
+  if (value === undefined || value === null) {
+    // 如果是 null 或 undefined，直接返回 false
+    return false
+  }
+
+  const number = typeof value === 'number' ? value : Number.parseFloat(value)
+
+  // 确保转换后的值是有效数字
+  return !Number.isNaN(number) && number < 0
+}
+
 export * from './tree'
 
 export { storage }
