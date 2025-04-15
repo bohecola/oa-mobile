a34eecd787a0ce3ad532e1de86c4cb26bae60bf1
li.kong <bohecola@outlook.com>
2025-04-15 15:33:16 +0800

    :bug: fix: icp link

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -7,16 +7,22 @@ const { appTitle } = useGlobSettings()
 </script>
 
 <template>
-  <div class="h-screen flex justify-center p-safe-offset-8">
-    <div class="w-full flex flex-col">
-      <div class="flex flex-col items-center justify-center">
-        <Logo class="mb-8 mt-8" />
-        <div class="text-2xl mb-12 font-black">
-          {{ appTitle }}
-        </div>
-
-        <LoginForm />
+  <div class="h-screen flex flex-col justify-between p-safe-offset-8">
+    <div class="flex flex-col items-center">
+      <Logo class="mt-2 mb-8" />
+      <div class="text-2xl mb-8 font-black">
+        {{ appTitle }}
       </div>
+
+      <LoginForm />
     </div>
+
+    <a
+      href="https://beian.miit.gov.cn/#/Integrated/index"
+      target="_blank"
+      class="mt-3 text-xs text-center text-gray-500"
+    >
+      备案号：陕ICP备2024032973号
+    </a>
   </div>
 </template>
